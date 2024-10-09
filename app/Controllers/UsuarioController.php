0170793484e45a35183d38ebd57931d8c040605b
Paola Alejandra Apaza Quispe <paolaapaza57@gmail.com>
2024-10-09 10:37:29 -0400

    version de Pao

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -8,17 +8,17 @@ use App\Models\UsuarioModel;
 
 class UsuarioController extends BaseController
 {
-    public function index()
-    {
-        $usuarioModel = new UsuarioModel();
-        // $usuarioModel2 = model('UsuarioModel');
-        $usuarioModel->insert([
-            'nombre' => 'Juan',
-            'fecha_creacion' => date('Y-m-d H:i:s'),
-            'estado' => 'ACTIVO'
-        ]);
-        $usuarios = $usuarioModel->findAll();
-        return dd($usuarios);
-        return view('usuarios/index');
-    }
+  public function index()
+  {
+    $usuarioModel = new UsuarioModel();
+    // $usuarioModel2 = model('UsuarioModel');
+    $usuarioModel->insert([
+      'nombre' => 'Juan',
+      'fecha_creacion' => date('Y-m-d H:i:s'),
+      'estado' => true
+    ]);
+    $usuarios = $usuarioModel->findAll();
+    // return dd($usuarios);
+    return view('usuarios/index', ["usuarios" => $usuarios]);
+  }
 }
